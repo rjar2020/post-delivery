6f5d8c1557cb5f217e1d3d286656951731e4f919
rolando.angelini <rolando.angelini@zopa.com>
2020-09-11 22:32:09 +0200

    Improvements to docker image

--- start-postback-in-image.sh
+++ start-postback-in-image.sh
@@ -2,4 +2,4 @@ source ~/.profile
 cd /home/rjar/
 /home/rjar/kafka/kafka_2.12-2.6.0/bin/zookeeper-server-start.sh /home/rjar/kafka/kafka_2.12-2.6.0/config/zookeeper.properties > /home/rjar/kafka/kafka_2.12-2.6.0/zookeper.log 2>&1&
 /home/rjar/kafka/kafka_2.12-2.6.0/bin/kafka-server-start.sh /home/rjar/kafka/kafka_2.12-2.6.0/config/server.properties > /home/rjar/kafka/kafka_2.12-2.6.0/kafka.log 2>&1&
-go run github.com/rjar2020/post-delivery
+/usr/local/go/bin/go run github.com/rjar2020/post-delivery
