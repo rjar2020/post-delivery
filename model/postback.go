4c1ad2b0244e97eec7d8264c8bbfb4901ffd65c0
rolando.angelini <rolando.angelini@zopa.com>
2020-10-03 20:06:43 +0200

    Error handling refactor

--- model/postback.go
+++ model/postback.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"encoding/json"
-	"log"
 )
 
 //PostBack maps a postback message comming from the ingestors
@@ -15,8 +14,5 @@ type PostBack struct {
 func FromJSONtoPostback(payload []byte) (PostBack, error) {
 	var postBack PostBack
 	err := json.Unmarshal(payload, &postBack)
-	if err != nil {
-		log.Printf("Error decoding kafka message: %s", err)
-	}
 	return postBack, err
 }
